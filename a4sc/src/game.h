b6210aee3f9fbb00d31c65a0ef91a29029074c84
Juan Orozco <eliasyfgm@gmail.com>
2016-02-10 21:28:13 -0600

    Some readjustments

--- a4sc/src/game.h
+++ a4sc/src/game.h
@@ -6,10 +6,6 @@
 #define C_BLACK  makecol(0, 0, 0)
 #define C_WHITE  makecol(255, 255, 255)
 
-// Bounding box collision
-#define check_bb_collision(x1,y1,w1,h1,x2,y2,w2,h2) (!( ((x1)>=(x2)+(w2)) || ((x2)>=(x1)+(w1)) || \
-                                                        ((y1)>=(y2)+(h2)) || ((y2)>=(y1)+(h1)) ))
-
 struct Game_Config
 {
     int width;
@@ -28,9 +24,13 @@ int game_init(struct Game_Config* config, const char* title);
 void game_run();
 void game_over();
 
-// State functions
+// Main state routines
 void change_state(struct State* state, void* param);
 void push_state(struct State* state, void* param);
 void pop_state();
+
+// Bounding box collision
+#define check_bb_collision(x1,y1,w1,h1,x2,y2,w2,h2) (!( ((x1)>=(x2)+(w2)) || ((x2)>=(x1)+(w1)) || \
+                                                        ((y1)>=(y2)+(h2)) || ((y2)>=(y1)+(h1)) ))
 
 #endif // GAME_H_INCLUDED
