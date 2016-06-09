02824dd0044f504bd023528cf35302795c01cd44
eliasYFGM <eliasyfgm@gmail.com>
2016-06-09 15:21:26 -0500

    Added transparent color macro

--- a4sc/src/game.h
+++ a4sc/src/game.h
@@ -1,8 +1,9 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
-#define C_BLACK  makecol(0, 0, 0)
-#define C_WHITE  makecol(255, 255, 255)
+#define C_BLACK   makecol(0, 0, 0)
+#define C_WHITE   makecol(255, 255, 255)
+#define C_TRANS   makecol(255, 0, 255)
 
 struct Game_Config
 {
